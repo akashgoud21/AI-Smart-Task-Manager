273f44f1ba984fce720f374df579f60d8a1effbb
akashgoud21 <goud6414@gmail.com>
2026-06-19 21:28:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///taskmanager.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
+with app.app_context():
+    db.create_all()
 
 def get_ai_suggestion(priority):
 
@@ -202,8 +203,4 @@ def delete(id):
 
 
 if __name__ == '__main__':
-
-    with app.app_context():
-        db.create_all()
-
     app.run(debug=True)
